260e94c2ef2dc50244c55028e614527e9b01ca75
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-06 20:08:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,10 +15,10 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.sort do |a,b|
+  new_array = array.sort do |a,b|
     a = array[1] 
     b = array[2]
     a,b = b,a 
   end 
-    array 
+    new_arrayarray 
 end 
