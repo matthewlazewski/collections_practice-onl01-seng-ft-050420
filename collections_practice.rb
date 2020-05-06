3ca99e2742c6c528cb507d64cc147573fb80f5d0
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-06 20:24:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,7 +32,8 @@ def kesha_maker(array)
 end 
 
 def find_a(array)
-  new_array= array.sort do |name|
+  new_array = []
+  array.sort do |name|
     if name.starts_with?("a")
     new_array << name
   end
